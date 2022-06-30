fc11ab84a664b04a42a66b53e08b6d2b21e8d63c
julietabueno <bueno.julieta.ok@gmail.com>
2022-06-30 14:00:30 -0300

    ordene las verificaciones de los ingresos

--- script.js
+++ script.js
@@ -8,20 +8,18 @@ const calculo = (pesos, valorC) => pesos / valorC;
 
 do {
     pesos = parseFloat(prompt("Ingrese la cantidad de pesos que desea Invertir"));
-    crypto = (prompt("Ingrese la cryptomoneda que desea adquirir: BTC, ETH o USDC")).toLowerCase();
 
     //Pido otro monto si no es un numero
     if (isNaN(pesos)) {
         alert("Por favor ingrese un monto válido.")
     }
 
+    crypto = (prompt("Ingrese la cryptomoneda que desea adquirir: BTC, ETH o USDC")).toLowerCase();
+
     // pido que ingrese nuevamente la cryptomoneda si no es valida
     if ((crypto != "btc") && (crypto != "eth") && (crypto != "usdc")) {
         alert("Por favor ingrese una cryptomoneda válida.")
     }
-
-} while (pesos >= 0) {
-
     //le doy un valor a cada crypto
     switch (crypto) {
         case "btc":
@@ -36,6 +34,10 @@ do {
         default:
             alert("La compra no pudo ser realizada")
     }
+
+} while (pesos >= 0) {
+
+
     //Me da el mensaje de cuanto puedo comprar de la moneda elegida
-    alert(`Usted esta por comprar ${calculo(pesos,valorC)} ${(crypto).toUpperCase()}`)
+    alert(`Usted esta por comprar ${calculo(pesos, valorC)} ${(crypto).toUpperCase()}`)
 }
