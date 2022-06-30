b0877c3bd26cabd177ac6d83b4dc220a3b064d46
julietabueno <bueno.julieta.ok@gmail.com>
2022-06-30 13:32:05 -0300

    arranque de nuevo

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 //Desafio 2
 
-//Declaro la funcion dividir
-const dividir = (num1, num2) => num1 / num2
+//declaro las variables
+let pesos, crypto, valorC
 
-//declro las variables
-let pesos = 0
-const valorBtc = 4951255.45
-const valorEth = 284295.28
-const valorDai = 239
-const valorUsdc = 239.29
-let resultado = ''
+//creo la funcion para sacar el precio de la moneda para dividir los $
+const calculo = (pesos, valorC) => pesos / valorC;
 
-do {//Pido al usuario que ingrese la cantidad de $ que desea invertir y que moneda quiere comprar
+do {
     pesos = parseFloat(prompt("Ingrese la cantidad de pesos que desea Invertir"));
-    crypto = prompt("Ingrese la cryptomoneda que desea adquirir: BTC, ETH, DAI o USDC").toLowerCase();
+    crypto = prompt("Ingrese la cryptomoneda que desea adquirir: BTC, ETH o USDC").toLowerCase();
 
     //Pido otro monto si no es un numero
     if (isNaN(pesos)) {
         alert("Por favor ingrese un monto válido.")
     }
+
     // pido que ingrese nuevamente la cryptomoneda si no es valida
-    if ((crypto != "btc") && (crypto != "eth") && (crypto != "dai") && (crypto != "usdc")) {
+    if ((crypto != "btc") && (crypto != "eth") && (crypto != "usdc")) {
         alert("Por favor ingrese una cryptomoneda válida.")
     }
-    //hago la cuenta de la moneda
+
+    //le doy un valor a cada crypto
     switch (crypto) {
         case "btc":
-            alert(`Usted va a comprar ${dividir(pesos, valorBtc)} BTC`)
+            valorC = 4951255.45
             break
         case "eth":
-            alert(`Usted va a comprar ${dividir(pesos, valorEth)} ETH`)
-            break
-        case "dai":
-            alert(`Usted va a comprar ${dividir(pesos, valorDai)} DAI`)
+            valorC = 284295.28
             break
         case "usdc":
-            alert(`Usted va a comprar ${dividir(pesos, valorUsdc)} USDC`)
+            valorC = 239
             break
         default:
             alert("La compra no pudo ser realizada")
     }
 
-} while (pesos >= 0);
+
+
+} while (pesos >= 0) {
+
+    //Me da el mensaje de cuanto puedo comprar de la moneda elegida
+    alert(`Usted esta por comprar ${calculo} ${(crypto).toUpperCase()}`)
+}
